07cabc012b2d28edc7a6592ad05c68d4d95d0d4f
Julian Clémot <>
2024-05-10 16:59:58 +0200

    :sparkles: retrieve list of characters and display it

--- app/src/main/java/io/mobilisinmobile/disneyworld/MainActivity.kt
+++ app/src/main/java/io/mobilisinmobile/disneyworld/MainActivity.kt
@@ -1,15 +1,31 @@
 package io.mobilisinmobile.disneyworld
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import coil.transform.CircleCropTransformation
+import io.mobilisinmobile.disneyworld.CharactersAdapter.CharacterViewHolder
 import io.mobilisinmobile.disneyworld.databinding.ActivityMainBinding
+import io.mobilisinmobile.disneyworld.databinding.ItemCharacterBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val disneyService = GetCharactersUseCase()
+    private val adapter = CharactersAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.enableEdgeToEdge()
@@ -21,5 +37,67 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        GlobalScope.launch {
+            val characters = disneyService.getCharacters().characters
+            runOnUiThread{
+                adapter.submitList(characters)
+            }
+        }
+    }
+}
+
+class CharactersAdapter : ListAdapter<CharacterData, CharacterViewHolder>(CharacterDiffUtil()) {
+    class CharacterDiffUtil : DiffUtil.ItemCallback<CharacterData>() {
+        override fun areItemsTheSame(oldItem: CharacterData, newItem: CharacterData): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: CharacterData, newItem: CharacterData): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    class CharacterViewHolder(
+        private val binding: ItemCharacterBinding,
+    ) : ViewHolder(binding.root) {
+        constructor(parent: ViewGroup) : this(
+            ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+        fun bind(item: CharacterData) {
+            binding.apply {
+                avatar.load(item.imageUrl) {
+                    crossfade(true)
+                    transformations(CircleCropTransformation())
+                }
+
+                name.text = item.name
+
+                val appearancesInMovie = item.films + item.shortFilms + item.tvShows
+
+                val stringBuilder = StringBuilder()
+                appearancesInMovie.forEachIndexed { index, appearance ->
+                    stringBuilder.append(appearance)
+                    if (index < appearancesInMovie.size - 1) {
+                        stringBuilder.append(", ")
+                    }
+                }
+
+                appearances.text = stringBuilder.toString()
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        return CharacterViewHolder(parent)
     }
 }
