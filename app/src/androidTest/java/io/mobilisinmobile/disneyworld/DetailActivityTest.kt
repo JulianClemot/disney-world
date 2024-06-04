43e2a20736f378a315293b75d085ff13a1838b33
Julian Clémot <>
2024-06-04 13:52:38 +0200

    :sparkles: add empty functional tests for detail activity

---
+++ app/src/androidTest/java/io/mobilisinmobile/disneyworld/DetailActivityTest.kt
@@ -0,0 +1,22 @@
+package io.mobilisinmobile.disneyworld
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DetailActivityTest {
+
+    @Test
+    fun should_on_create_display_character_detail_when_a_character_id_is_provided_to_the_screen_and_call_is_successful() {
+
+    }
+    @Test
+    fun should_on_create_display_error_message_when_no_character_id_is_provided_to_screen() {
+
+    }
+    @Test
+    fun should_on_create_display_error_message_when_when_a_character_id_is_provided_to_the_screen_and_call_is_failing() {
+
+    }
+}
